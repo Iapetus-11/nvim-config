962b36de4e7be7ea6d3a5869a44b4cb6111e8ae8
Milo Weinberg <git@iapetus11.me>
2026-08-02 09:57:11 -0400

    Improve rust support

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -1,10 +1,35 @@
 return {
   settings = {
     ["rust-analyzer"] = {
+      cargo = {
+        allTargets = true,
+        features = "all",
+      },
+
       check = {
         command = "clippy",
         extraArgs = { "--", "-A", "unused" },
-        features = "all",
+      },
+
+      rustc = { source = "discover" },
+
+      files = { excludeDirs = { "target", ".direnv" } },
+
+      imports = {
+        granularity = { group = "module" },
+        prefix = "self",
+      },
+
+      completion = {
+        callable = { snippets = "fill_arguments" },
+        fullFunctionSignatures = { enable = true },
+      },
+
+      inlayHints = {
+        closureReturnTypeHints = { enable = "with_block" },
+        chainingHints = { enable = true },
+        parameterHints = { enable = true },
+        maxLength = 25,
       },
     },
   },
