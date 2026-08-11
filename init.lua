53eb31037b06b8227fe1534a3c0685e569f95a08
Milo Weinberg <git@iapetus11.me>
2026-08-11 09:48:02 -0400

    Add terminal tab title + fix file reloading

--- init.lua
+++ init.lua
@@ -8,6 +8,10 @@ vim.opt.signcolumn = "yes"
 vim.opt.undofile = true
 vim.opt.clipboard = "unnamedplus"
 
+-- The terminal tab shows this title instead of a bare "nvim".
+vim.opt.title = true
+vim.opt.titlestring = "nvim - %{fnamemodify(getcwd(), ':t')}"
+
 vim.opt.laststatus = 3
 vim.opt.showmode = false
 vim.o.winborder = "rounded"
@@ -28,6 +32,16 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     if vim.bo.buftype == "help" and #vim.api.nvim_tabpage_list_wins(0) > 1 then
       vim.cmd("wincmd T")
+    end
+  end,
+})
+
+-- `autoread` only applies when a check is triggered, so trigger one on re-entry.
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  desc = "Reload files changed on disk",
+  callback = function()
+    if vim.o.buftype == "" then
+      vim.cmd.checktime()
     end
   end,
 })
