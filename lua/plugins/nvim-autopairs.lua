d8603d39a2eb4fed188a29949f8077dc4bf0a3c4
Milo Weinberg <git@iapetus11.me>
2026-09-01 08:27:00 -0400

    Update autopairs config

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,8 +1,5 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  opts = {
-    enable_check_bracket_line = true,
-    ignored_next_char = "[%w%.%]%)%}\"'`]",
-  },
+  opts = { check_ts = true },
 }
